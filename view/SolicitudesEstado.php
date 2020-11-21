1bf632bf8973be0c4cfaaff15404f681c2fd6db3
Luisbv14 <Luisbrenesv27@gmail.com>
2020-11-21 17:53:20 -0600

    Prueba

---
+++ view/SolicitudesEstado.php
@@ -0,0 +1,150 @@
+<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.0.8/css/all.css">
+<!-- Table with panel -->
+<div class="card card-cascade narrower">
+
+  <!--Card image-->
+  <div
+    class="view view-cascade gradient-card-header blue-gradient narrower py-2 mx-4 mb-3 d-flex justify-content-between align-items-center">
+
+    <div>
+      <button type="button" class="btn btn-outline-white btn-rounded btn-sm px-2">
+        <i class="fas fa-th-large mt-0"></i>
+      </button>
+      <button type="button" class="btn btn-outline-white btn-rounded btn-sm px-2">
+        <i class="fas fa-columns mt-0"></i>
+      </button>
+    </div>
+
+    <a class="white-text mx-3">Solicitudes</a>
+
+    <div>
+      <button type="button" class="btn btn-outline-white btn-rounded btn-sm px-2">
+        <i class="fas fa-pencil-alt mt-0"></i>
+      </button>
+      <button type="button" class="btn btn-outline-white btn-rounded btn-sm px-2">
+        <i class="far fa-trash-alt mt-0"></i>
+      </button>
+      <button type="button" class="btn btn-outline-white btn-rounded btn-sm px-2">
+        <i class="fas fa-info-circle mt-0"></i>
+      </button>
+    </div>
+
+  </div>
+  <!--/Card image-->
+
+  <div class="px-4">
+
+    <div class="table-wrapper">
+      <!--Table-->
+      <table class="table table-hover mb-0">
+
+        <!--Table head-->
+        <thead>
+          <tr>
+            <th>
+
+              <label class="form-check-label" for="checkbox" class="mr-2 label-table"></label>
+            </th>
+            <th class="th-lg">
+              <a>Costo
+                <i class="fas fa-sort ml-1"></i>
+              </a>
+            </th>
+            <th class="th-lg">
+              <a href="">Link
+                <i class="fas fa-sort ml-1"></i>
+              </a>
+            </th>
+            <th class="th-lg">
+              <a href="">Razon
+                <i class="fas fa-sort ml-1"></i>
+              </a>
+            </th>
+            <th class="th-lg">
+              <a href="">Solicitador
+                <i class="fas fa-sort ml-1"></i>
+              </a>
+            </th>
+            <th class="th-lg">
+              <a href="">Descripcion
+                <i class="fas fa-sort ml-1"></i>
+              </a>
+            </th>
+            <th class="th-lg">
+              <a href="">Estado
+                <i class="fas fa-sort ml-1"></i>
+              </a>
+            </th>
+          </tr>
+        </thead>
+        <!--Table head-->
+
+        <!--Table body-->
+        <tbody>
+          <tr>
+            <th scope="row">
+              <label class="form-check-label" for="checkbox1" class="label-table"></label>
+            </th>
+            <td>Monto</td>
+            <td>URL</td>
+            <td>Justificacion</td>
+            <td>Username</td>
+            <td>Descripcion</td>
+            <td>Estado</td>
+          </tr>
+          <tr>
+            <th scope="row">
+              <label class="form-check-label" for="checkbox2" class="label-table"></label>
+            </th>
+            <td>Monto</td>
+            <td>URL</td>
+            <td>Justificacion</td>
+            <td>Username</td>
+            <td>Descripcion</td>
+            <td>Estado</td>
+
+          </tr>
+          <tr>
+            <th scope="row">
+              <label class="form-check-label" for="checkbox3" class="label-table"></label>
+            </th>
+            <td>Monto</td>
+            <td>URL</td>
+            <td>Justificacion</td>
+            <td>Username</td>
+            <td>Descripcion</td>
+            <td>Estado</td>
+          </tr>
+          <tr>
+            <th scope="row">
+              <label class="form-check-label" for="checkbox4" class="label-table"></label>
+            </th>
+            <td>Monto</td>
+            <td>URL</td>
+            <td>Justificacion</td>
+            <td>Username</td>
+            <td>Descripcion</td>
+            <td>Estado</td>
+          </tr>
+          <tr>
+            <th scope="row">
+              <label class="form-check-label" for="checkbox5" class="label-table"></label>
+            </th>
+            <td>Monto</td>
+            <td>URL</td>
+            <td>Justificacion</td>
+            <td>Username</td>
+            <td>Descripcion</td>
+            <td>Estado</td>
+
+          </tr>
+        </tbody>
+        <!--Table body-->
+      </table>
+      <!--Table-->
+    </div>
+
+  </div>
+
+</div>
+<!-- Table with panel -->
