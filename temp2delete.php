614fc2a47d31661f385e2a89e6a3a7bc693f908b
badervalsan72 <ebadillav27@gmail.com>
2020-12-11 10:23:46 -0600

    emergency commit

---
+++ temp2delete.php
@@ -0,0 +1,142 @@
+// conexion con la db
+// extraer total de productos ( select COUNT(ID) y almacenar en $variable)
+// crear un for para $variable que itere sobre todos los productos y cree un elemento gráfico.
+
+
+
+<div class="col-md-4 col-sm-6 col-xs-12 gallery-item-wrapper dulce">
+	<div class="gallery-item">
+		<div class="gallery-thumb">
+			<img src="img/works/1.png" class="img-responsive" alt="1st gallery Thumb">
+			<div class="image-overlay"></div>
+			<a href="img/works/1.png" class="gallery-zoom"><i class="fa fa-eye"></i></a>
+
+		</div>
+		<div class="gallery-details">
+			<div class="editContent">
+				<h5>Dona con glaseado azucarado</h5>
+			</div>
+			<div class="editContent">
+				<p>Insertar precio</p>
+			</div>
+			<button type="submit" style="margin:10px;" class="btn btn-primary waves-effect waves-dark pull-center">Comprar</button>
+		</div>
+	</div>
+</div>
+
+
+<div class="col-md-4 col-sm-6 col-xs-12 gallery-item-wrapper dulce">
+	<div class="gallery-item">
+		<div class="gallery-thumb">
+			<img src="img/works/6.png" class="img-responsive" alt="1st gallery Thumb">
+			<div class="image-overlay"></div>
+			<a href="img/works/6.png" class="gallery-zoom"><i class="fa fa-eye"></i></a>
+
+		</div>
+		<div class="gallery-details">
+			<div class="editContent">
+				<h5>Tostada francesa</h5>
+			</div>
+			<div class="editContent">
+				<p>Insertar precio</p>
+			</div>
+			<button type="submit" style="margin:10px;" class="btn btn-primary waves-effect waves-dark pull-center">Comprar</button>
+		</div>
+	</div>
+</div>
+<div class="col-md-4 col-sm-6 col-xs-12 gallery-item-wrapper dulce">
+	<div class="gallery-item">
+		<div class="gallery-thumb">
+			<img src="img/works/3.png" class="img-responsive" alt="1st gallery Thumb">
+			<div class="image-overlay"></div>
+			<a href="img/works/3.png" class="gallery-zoom"><i class="fa fa-eye"></i></a>
+
+		</div>
+		<div class="gallery-details">
+			<div class="editContent">
+				<h5>Queque de fiesta</h5>
+			</div>
+			<div class="editContent">
+				<p>Insertar precio</p>
+			</div>
+			<button type="submit" style="margin:10px;" class="btn btn-primary waves-effect waves-dark pull-center">Comprar</button>
+		</div>
+	</div>
+</div>
+
+
+
+
+
+
+// wrapers salados.
+
+
+
+
+
+
+
+
+
+
+
+
+
+<div class="col-md-4 col-sm-6 col-xs-12 gallery-item-wrapper salado">
+	<div class="gallery-item">
+		<div class="gallery-thumb">
+			<img src="img/works/4.png" class="img-responsive" alt="1st gallery Thumb">
+			<div class="image-overlay"></div>
+			<a href="img/works/4.png" class="gallery-zoom"><i class="fa fa-eye"></i></a>
+
+		</div>
+		<div class="gallery-details">
+			<div class="editContent">
+				<h5>Tortillas caseras</h5>
+			</div>
+			<div class="editContent">
+				<p>Insertar precio</p>
+			</div>
+			<button type="submit" style="margin:10px;" class="btn btn-primary waves-effect waves-dark pull-center">Comprar</button>
+		</div>
+	</div>
+</div>
+<div class="col-md-4 col-sm-6 col-xs-12 gallery-item-wrapper salado">
+	<div class="gallery-item">
+		<div class="gallery-thumb">
+			<img src="img/works/5.png" class="img-responsive" alt="1st gallery Thumb">
+			<div class="image-overlay"></div>
+			<a href="img/works/5.png" class="gallery-zoom"><i class="fa fa-eye"></i></a>
+
+		</div>
+		<div class="gallery-details">
+			<div class="editContent">
+				<h5>Tamal</h5>
+			</div>
+			<div class="editContent">
+				<p>Insertar precio</p>
+			</div>
+			<button type="submit" style="margin:10px;" class="btn btn-primary waves-effect waves-dark pull-center">Comprar</button>
+		</div>
+	</div>
+</div>
+<div class="col-md-4 col-sm-6 col-xs-12 gallery-item-wrapper salado">
+	<div class="gallery-item">
+		<div class="gallery-thumb">
+			<img src="img/works/2.png" class="img-responsive" alt="1st gallery Thumb">
+			<div class="image-overlay"></div>
+			<a href="img/works/2.png" class="gallery-zoom"><i class="fa fa-eye"></i></a>
+
+		</div>
+		<div class="gallery-details">
+			<div class="editContent">
+				<h5>Pan Baguette</h5>
+			</div>
+			<div class="editContent">
+				<p>Insertar precio</p>
+			</div>
+			<button type="submit" style="margin:10px;" class="btn btn-primary waves-effect waves-dark pull-center">Comprar</button>
+		</div>
+	</div>
+</div>
