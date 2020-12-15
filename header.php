fb13f3aa91edc893888046ed262da589ca0f1d5e
Luisbv14 <Luisbrenesv27@gmail.com>
2020-12-15 01:34:47 -0600

    Pequeño cambio en la interfaz del header, cambio del nombre de la aplicacion

--- header.php
+++ header.php
@@ -8,7 +8,7 @@
                         <span class="icon-bar"></span>
                         <span class="icon-bar"></span>
                     </button>
-                    <a class="navbar-brand" href="index.php"><i class="icon-info-blocks material-icons">location_on</i>Panaderia</a>
+                    <a class="navbar-brand" href="index.php"><i class="icon-info-blocks material-icons">location_on</i>Panadería ULACIT</a>
                 </div>
                 <div class="navbar-collapse collapse">
                     <ul class="nav navbar-nav">
