029bbbbf85c2b8d2d811a5537fc1243c8c58fa6d
badervalsan72 <ebadillav27@gmail.com>
2020-12-16 09:42:33 -0600

    cart

--- includes/cart-inc.php
+++ includes/cart-inc.php
@@ -1,21 +1,6 @@
 <?php
-        /*
-        if(array_key_exists('button1', $_POST)) { 
-            button1(); 
-        } 
-        else if(array_key_exists('button2', $_POST)) { 
-            button2(); 
-        } 
-        function button1() { 
-            echo "This is Button1 that is selected"; 
-        } 
-        function button2() { 
-            echo "This is Button2 that is selected"; 
-        } 
-        */
-
     function eliminar($i) {
-        print_r("hola" . $i); 
-        array_splice($_SESSION["productList"], $i, 1); 
+        
+        array_splice($_SESSION["productList"], $i, 1);
+        header("Refresh:0");
     }
-    ?> 
